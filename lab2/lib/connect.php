d11548a8e9be61e95f20792b358ba25b17abd4ee
F1lterEr <98636299+F1lterEr@users.noreply.github.com>
2026-01-17 05:11:57 +0300

    Delete lab2 directory

--- lab2/lib/connect.php
+++
@@ -1,6 +0,0 @@
-<?php  
-$link = mysqli_connect("localhost", "root", "root")  
-    or die ("Ошибка подключения к базе данных");  
-mysqli_set_charset($link, "utf8");  
-mysqli_select_db($link, "mysite");  
-?>
